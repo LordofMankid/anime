fbaf3424418d039f47774357c570361bc8535f81
jsihuang <jessie910130@gmail.com>
2022-12-01 01:41:26 -0500

    added carousel images and fixed some animations

---
+++ pages/home.js
@@ -0,0 +1,20 @@
+var carouselWidth = $('.carousel-inner')[0].scrollWidth;
+var cardWidth = $('.carousel-item').width();
+
+var scrollPosition = 0;
+
+$('.carousel-control-next').on('click', function(){
+  if(scrollPosition < (carouselWidth - (cardWidth * 4))){
+    console.log('next');
+    scrollPosition += cardWidth;
+    $('.carousel-inner').animate({scrollLeft:scrollPosition}, 600);
+  }
+});
+
+$('.carousel-control-prev').on('click', function(){
+  if(scrollPosition > 0){
+    console.log('prev');
+    scrollPosition -= cardWidth;
+    $('.carousel-inner').animate({scrollLeft:scrollPosition}, 600);
+  }
+});
