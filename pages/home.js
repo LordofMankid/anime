a776edd97711bcf6de63ce87b6a6ea999f0282c0
yihui <90987235+yihui-hu@users.noreply.github.com>
2022-12-01 20:23:50 -0500

    image carousel now populated with real data, moved css for home, login and signup into css styles, home.js is now called explicitly in async function in home.html

--- pages/home.js
+++ pages/home.js
@@ -1,20 +1,23 @@
-var carouselWidth = $('.carousel-inner')[0].scrollWidth;
-var cardWidth = $('.carousel-item').width();
+function initialiseCarousel() {
+  console.log("Initializing")
+  var carouselWidth = $('.carousel-inner')[0].scrollWidth;
+  var cardWidth = $('.carousel-item').width();
 
-var scrollPosition = 0;
+  var scrollPosition = 0;
 
-$('.carousel-control-next').on('click', function(){
-  if(scrollPosition < (carouselWidth - (cardWidth * 4))){
-    console.log('next');
-    scrollPosition += cardWidth;
-    $('.carousel-inner').animate({scrollLeft:scrollPosition}, 600);
-  }
-});
+  $('.carousel-control-next').on('click', function () {
+    if (scrollPosition < (carouselWidth - (cardWidth * 4))) {
+      console.log('next');
+      scrollPosition += cardWidth;
+      $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+    }
+  });
 
-$('.carousel-control-prev').on('click', function(){
-  if(scrollPosition > 0){
-    console.log('prev');
-    scrollPosition -= cardWidth;
-    $('.carousel-inner').animate({scrollLeft:scrollPosition}, 600);
-  }
-});
+  $('.carousel-control-prev').on('click', function () {
+    if (scrollPosition > 0) {
+      console.log('prev');
+      scrollPosition -= cardWidth;
+      $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+    }
+  });
+}
