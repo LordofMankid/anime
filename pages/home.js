e30bb01ece4698efc3f54d2f16eed00218e81aad
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2022-12-03 12:39:04 -0500

    edited buttons/fixed carousel scrolling

--- pages/home.js
+++ pages/home.js
@@ -3,20 +3,38 @@ function initialiseCarousel() {
   var cardWidth = $('.carousel-item').width();
 
   var scrollPosition = 0;
+  var scrollPosition2 = 0;
 
-  $('.carousel-control-next').on('click', function () {
+  $('#next-slide').on('click', function () {
     if (scrollPosition < (carouselWidth - (cardWidth * 4))) {
       console.log('next');
       scrollPosition += cardWidth;
-      $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+      $('#carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
     }
   });
 
-  $('.carousel-control-prev').on('click', function () {
+  $('#prev-slide').on('click', function () {
     if (scrollPosition > 0) {
       console.log('prev');
       scrollPosition -= cardWidth;
-      $('.carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+      $('#carousel-inner').animate({ scrollLeft: scrollPosition }, 600);
+    }
+  });
+
+  $('#next-slide-2').on('click', function () {
+    if (scrollPosition2 < (carouselWidth - (cardWidth * 4))) {
+      console.log('next');
+      scrollPosition2 += cardWidth;
+      $('#carousel-inner-2').animate({ scrollLeft: scrollPosition2 }, 600);
+    }
+  });
+
+  $('#prev-slide-2').on('click', function () {
+    if (scrollPosition2 > 0) {
+      console.log('prev');
+      scrollPosition2 -= cardWidth;
+      $('#carousel-inner-2').animate({ scrollLeft: scrollPosition2 }, 600);
     }
   });
 }
+
